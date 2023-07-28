88b11e605b8e5b6c120dcae09a09f88b5734139b
mondousage <casspangell@gmail.com>
2023-07-28 16:17:39 -0600

    Update recaptcha.php

--- recaptcha.php
+++ recaptcha.php
@@ -1,18 +1,12 @@
 <?php   
  
-//Setup connection variables, such as database username 
-//and password 
-
 $hostname=""; 
 $username=""; 
 $password=""; 
 $dbname=""; 
 $tablename="";
-
-// Create a new connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -22,18 +16,16 @@ $checkSql = "SELECT `recaptcha-site-key` FROM $tablename WHERE name = '$name'";
 $result = $conn->query($checkSql);
 
 if ($result) {
-    // Fetch the data from the first row (assuming there is only one row for the given name)
     $row = mysqli_fetch_assoc($result);
 
     if ($row) {
-        // Echo the value of the "recaptcha-site-key" column
         $recaptchaSiteKey = $row['recaptcha-site-key'];
         echo $recaptchaSiteKey;
     } else {
-        // Handle the case when no rows are found for the given name
         echo "No record found for the name: $name";
     }
 }
+
 
 
 // Close the connection

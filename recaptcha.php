850c779de3f7b3106fbd87740c73593fa64d1aef
mondousage <casspangell@gmail.com>
2023-07-28 16:10:02 -0600

    Create recaptcha.php

---
+++ recaptcha.php
@@ -0,0 +1,41 @@
+<?php   
+ 
+//Setup connection variables, such as database username 
+//and password 
+
+$hostname="74.124.198.128"; 
+$username="asoulh5"; 
+$password="\$Mi11ion\$123"; 
+$dbname="asoulh5_zoom"; 
+$tablename="zoomcreds";
+
+// Create a new connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = 'wellspringoflight';
+$checkSql = "SELECT `recaptcha-site-key` FROM $tablename WHERE name = '$name'";
+$result = $conn->query($checkSql);
+
+if ($result) {
+    // Fetch the data from the first row (assuming there is only one row for the given name)
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row) {
+        // Echo the value of the "recaptcha-site-key" column
+        $recaptchaSiteKey = $row['recaptcha-site-key'];
+        echo $recaptchaSiteKey;
+    } else {
+        // Handle the case when no rows are found for the given name
+        echo "No record found for the name: $name";
+    }
+}
+
+
+// Close the connection
+$conn->close();
+?>
